50a8ea88dcdcaa336fd25649efbfe0d3357e9448
Izzat <work@izzatalsharif.com>
2025-01-15 17:06:50 +0200

    CB-14 Add JsonSchema logic to DefaultGenerationService

--- app/application/generation.py
+++ app/application/generation.py
@@ -1,3 +1,8 @@
+import json
+
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from langchain_core.prompts import PromptTemplate
+
 from app.application.abstract.langchain import ChatModelFactory
 from app.application.abstract.services import GenerationService
 from app.domain.data import TextData
@@ -9,10 +14,27 @@ class DefaultGenerationService(GenerationService):
         self.chat_model_factory = chat_model_factory
 
     async def generate(self, request: GenerationRequest) -> GenerationResponse:
-        llm = self.chat_model_factory.get_chat_model()
+        model = self.chat_model_factory.get_chat_model()
+        response_json_schema = request.options.response_json_schema
 
-        chat_response = await llm.ainvoke(request.input.text)
+        if response_json_schema is None or len(response_json_schema) == 0:
+            prompt = PromptTemplate(
+                template="{query}",
+                input_variables=["query"],
+            )
+            chain = prompt | model | StrOutputParser()
+        else:
+            schema_dict = json.loads(response_json_schema)
+            parser = JsonOutputParser(schema=schema_dict)
+            prompt = PromptTemplate(
+                template="{query}\n{format_instructions}",
+                input_variables=["query"],
+                partial_variables={"format_instructions": parser.get_format_instructions()},
+            )
+            chain = prompt | model | parser
+
+        response = await chain.ainvoke({"query": request.input.text})
 
         return GenerationResponse(
-            generated_output=TextData(text=chat_response.content)
+            generated_output=TextData(text=str(response))
         )
